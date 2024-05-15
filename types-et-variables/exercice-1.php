cbba5e62a0c6b9aeea4e23c23ed912814f3e13a4
Marion1b <marion.barthoux@gmail.com>
2024-05-15 10:56:50 +0200

    exercice 2 types et variables

--- types-et-variables/exercice-1.php
+++ types-et-variables/exercice-1.php
@@ -1,8 +1,13 @@
 <?php
     $nb = 42;
     echo $nb;
+    echo "<br>";
+
     $str = '42';
     echo $str;
+    echo "<br>";
+    
     $nbstr = (string) $nb;
     echo $nbstr;
+    echo "<br>";
 ?>
