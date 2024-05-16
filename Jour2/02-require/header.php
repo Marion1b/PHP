ca2a59e934385f09d55eed3cb76d0632f7fc3838
Marion1b <marion.barthoux@gmail.com>
2024-05-16 12:21:08 +0200

    exercice require jour 2

---
+++ Jour2/02-require/header.php
@@ -0,0 +1,16 @@
+
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="http://bre02php/Jour2/02-require/index.php?route=home">Accueil</a>
+            </li>
+            <li>
+                <a href="http://bre02php/Jour2/02-require/index.php?route=about">À propos</a>
+            </li>
+            <li>
+                <a href="http://bre02php/Jour2/02-require/index.php?route=contact">Contact</a>
+            </li>
+        </ul>
+    </nav>
+</header>
