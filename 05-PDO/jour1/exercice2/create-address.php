ccce8ddd8d996cd1fc509d569d03a6883bdd2b2b
Marion1b <marion.barthoux@gmail.com>
2024-05-24 11:51:43 +0200

    etape 6 : create address

---
+++ 05-PDO/jour1/exercice2/create-address.php
@@ -0,0 +1,36 @@
+<?php
+
+    require "connexion.php";
+
+    if(isset($_POST)){
+        $street = $_POST["street"];
+        $city = $_POST["city"];
+        $zipcode = $_POST["zipcode"];
+
+        $query = $db->prepare(
+            "INSERT INTO
+                address(
+                    id,
+                    street,
+                    city,
+                    zipcode
+                )
+                VALUES(
+                    NULL,
+                    :street,
+                    :city,
+                    :zipcode
+                )");
+
+        $parameters = [
+            ':street' => $street,
+            ':city' => $city,
+            ':zipcode' => $zipcode
+        ];
+
+        $query->execute($parameters);
+    }
+
+    
+
+?>
