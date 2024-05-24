5d228e5304308057e8f5c94ef7a12c4876d16334
Marion1b <marion.barthoux@gmail.com>
2024-05-24 12:16:47 +0200

    etape 7 : edit message

---
+++ 05-PDO/jour1/exercice2/edit-address.php
@@ -0,0 +1,32 @@
+<?php
+
+    require "connexion.php";
+
+    if(isset($_POST)){
+        $id = $_POST["id"];
+        $street = $_POST["street"];
+        $city = $_POST["city"];
+        $zipcode = $_POST["zipcode"];
+
+        $query = $db->prepare(
+            'UPDATE 
+                address
+            SET
+                street = :street,
+                city = :city,
+                zipcode = :zipcode
+            WHERE
+                id = :id
+        ');
+
+        $parameters = [
+            'id' => $id,
+            'street' => $street,
+            'city' => $city,
+            'zipcode' => $zipcode
+        ];
+
+        $query->execute($parameters);
+    }
+
+?>
